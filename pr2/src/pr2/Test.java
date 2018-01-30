58e390455f8a2de935f26faf813a091d0b359017
user <user@SHOUVIK>
2018-01-30 10:01:26 +0530

    bhbj

---
+++ pr2/src/pr2/Test.java
@@ -0,0 +1,33 @@
+package pr2;
+
+public class Test {
+
+	public static void main(String arg[])
+	{
+		if(false)
+			System.out.println("shouvik");
+		else if(true)
+			System.out.println("pradhan");
+		else
+				System.out.println("bal");
+		int j=(8>2)?9:8;
+		System.out.println(j);
+		/*
+		 * 
+		 * 
+		 * 
+		 */
+		String a="abc";
+		switch(a)
+		{
+		case "abc":
+			System.out.println("three");
+			break;
+		case "pqr":
+			System.out.println("four");
+			default:
+				System.out.println("select a valid option");
+		}
+		
+	}
+}
